f667fc924d62835730387d291385f1f80b738ae7
Jingxuan <taosit099@gmail.com>
2023-09-02 10:41:27 -0700

    Added remove filters button if filters are applied

---
+++ app/components/RemoveFiltersButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+const RemoveFiltersButton = ({className}: {className: string}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  if (!searchParams || searchParams.toString() === "") {
+    return null;
+  }
+
+  return (
+    <button 
+    className={className}
+    onClick={() => {
+        router.push("/");
+    }}
+    >
+      Remove all filters
+    </button>
+  )
+}
+
+export default RemoveFiltersButton
